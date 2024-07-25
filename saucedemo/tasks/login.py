1b8759eced1aed4cb6098088c0be294e2d8f4384
DiegoPhd <113406642+pruebasdiegofer@users.noreply.github.com>
2024-07-24 22:36:18 -0500

    login task

--- saucedemo/tasks/login.py
+++ saucedemo/tasks/login.py
@@ -1,2 +1,25 @@
+########## ONLY FOR LOCAL CONFIGURATION ##########
+import sys
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+folder_a_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+sys.path.append(folder_a_directory)
+
+##################################################
+
+from screenpy import Actor, beat
+from screenpy_selenium import Click, Enter
+
+from ui.page_home import LOGIN_BUTTON, PASSWORD_INPUT, USERNAME_INPUT
+from utils.constants import PASSWORD, USER
+
+
 class Login:
-    
+    @beat("{} logs in into saucelabs.com")
+    def perform_as(self, the_actor: Actor) -> None:
+        the_actor.attempts_to(
+            Enter.the_text(USER).into_the(USERNAME_INPUT),
+            Enter.the_text(PASSWORD).into_the(PASSWORD_INPUT),
+            Click.on(LOGIN_BUTTON),
+        )
