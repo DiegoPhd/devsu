6beff9c8dc29a9e6d5fab51e16c0f72b47b7a954
DiegoPhd <113406642+pruebasdiegofer@users.noreply.github.com>
2024-07-25 08:41:08 -0500

    finish v3 tests

--- saucedemo/tests/conftest.py
+++ saucedemo/tests/conftest.py
@@ -1,4 +1,21 @@
+from typing import Generator
+import pytest
+
+from screenpy import AnActor
 from screenpy.pacing import the_narrator
 from screenpy_adapter_allure import AllureAdapter
+from screenpy_selenium import BrowseTheWeb
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 the_narrator.adapters.append(AllureAdapter())
+
+
+@pytest.fixture
+def Diego() -> Generator:
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.set_window_size(1920, 1080)
+    the_actor = AnActor.named("Diego").who_can(BrowseTheWeb.using(driver))
+    yield the_actor
+    the_actor.exit()
--- saucedemo/tests/test_buy_products.py
+++ saucedemo/tests/test_buy_products.py
@@ -11,9 +11,6 @@ sys.path.append(folder_a_directory)
 from pytest_bdd import given, scenarios, then, when
 from screenpy import AnActor, ReadsExactly, See
 from screenpy_selenium import BrowseTheWeb, Open, Wait
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from ui.page_home import LOGIN_BUTTON
 from utils.constants import ORDER_COMPLETE, SAUCE_LABS_PAGE, TEN
 from ui.page_checkout import (
@@ -23,31 +20,28 @@ from tasks.login import Login
 from tasks.buy_products import BuyProducts
 from questions.the_confirm_order_text import TheConfirmOrderText
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.set_window_size(1920, 1080)
-the_actor = AnActor.named("Diego").who_can(BrowseTheWeb.using(driver))
-
 scenarios("../features/buy_products.feature")
 
 
 @given("a user visits saucedemo.com")
-def a_user_visits_saucedemo() -> None:
-    the_actor.attempts_to(Open.browser_on(SAUCE_LABS_PAGE))
-    the_actor.attempts_to(Wait(TEN).second_for(LOGIN_BUTTON))
+def a_user_visits_saucedemo(Diego: AnActor) -> None:
+    Diego.attempts_to(Open.browser_on(SAUCE_LABS_PAGE))
+    Diego.attempts_to(Wait(TEN).second_for(LOGIN_BUTTON))
 
 
 @given("the user logs in")
-def the_user_logs_in() -> None:
-    the_actor.attempts_to(Login())
+def the_user_logs_in(Diego: AnActor) -> None:
+    Diego.attempts_to(Login())
 
 
 @when("the user buy some products")
-def the_user_buy_some_product() -> None:
-    the_actor.attempts_to(BuyProducts())
+def the_user_buy_some_product(Diego: AnActor) -> None:
+    Diego.attempts_to(BuyProducts())
 
 
 @then("the user should see the order confirmation")
-def the_user_should_see_the_order_confirmation() -> None:
-    the_actor.attempts_to(Wait(TEN).second_for(COMPLETE_ORDER_TEXT))
-    the_actor.should(See.the(TheConfirmOrderText(), ReadsExactly(ORDER_COMPLETE)))
-    driver.quit()
+def the_user_should_see_the_order_confirmation(Diego: AnActor) -> None:
+    Diego.attempts_to(Wait(TEN).second_for(COMPLETE_ORDER_TEXT))
+    Diego.should(See.the(TheConfirmOrderText(), ReadsExactly(ORDER_COMPLETE)))
+    Diego.ability_to(BrowseTheWeb).browser.close()
+    Diego.exit_stage_left()
